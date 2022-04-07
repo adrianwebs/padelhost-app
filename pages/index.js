7acaea3edaeadf0e7c37b7337b1fe953aad7905e
Adrián Lozano Carretero <adriansoftwaredeveloper@gmail.com>
2022-04-07 18:26:18 +0200

    Imagenes añadadidas

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,48 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import logo from '../assets/images/logoblanco.png'
+
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Padelhost - Social Padel App</title>
         <meta name="description" content="Padelhost is the app that makes you save time, money and effort. It helps you easily reserve a court, find players to play with and locate the best places to play in your city. Want to book a court? Just open the app and reserve it with one click. Don't have a partner? Find one in the app, wherever you are. Are you traveling to another city? Find the best place to play there and make new friends!" />
-        <link rel="icon" href="../assets/images/logo negro.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+      <body className='app_body'>
+        <nav className='app_nav-container'>
+            <a href='#'><img src={logo}></img></a>
+            <div className='app_nav-items'>
+              <a className='app_nav-item' href='#features'>Features</a>
+              <a className='app_nav-item' href='#reviews'>Reviews</a>
+              <a className='app_nav-item' href='#blog'>Blog</a>
+              <a className='app_nav-item' href='#contact'>Contact</a>
+            </div>
+            <div className='app_nav-content'>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <nav>
-          <Link href='/login'><a>Login</a></Link>
+            </div>
         </nav>
-      </main>
+        <header>
+            <Link href='/login'><a>Login</a></Link>
+
+        </header>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+
+          <nav>
+          </nav>
+        </main>
+        <section>
+          
+        </section>
+      </body>
+      
 
       <footer className={styles.footer}>
         <a
