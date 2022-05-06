30248a52730c459390e9385d6e5a30584b523698
Adrián Lozano Carretero <adriansoftwaredeveloper@gmail.com>
2022-05-06 21:30:37 +0200

    Dashboard Created not stylized

---
+++ pages/components/navbar/NavbarDashboard.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+import Avatar from '../Avatar/Avatar'
+
+import styles from './styles.module.css'
+
+import {signOutUser} from '../../../firebase/client'
+
+import { motion } from 'framer-motion'
+
+function HomeNavbar ({username, avatar}) {
+
+  const handleSignOut = () => {
+    signOutUser()
+  }
+
+  return (
+    <>
+      <motion.div className={styles.navbar_container}
+        animate={{opacity: [0, 1], y:[-100, 0]}}
+        initial={{opacity: 0}}
+        transition={{duration: 1, ease: 'easeInOut'}}
+      >
+        <div className={styles.navbar_topnav}>
+          <div className={styles.navbar_title}>
+            <h2>PADELHOST</h2>
+            <span>/</span>
+            <strong>{username}</strong>
+          </div>
+          <div className={styles.navbar_actions}>
+            <button>Feedback</button>
+            <select>
+              <option hidden selected>Language</option>
+              <option value='en'>English</option>
+              <option value='es'>Spanish</option>
+              <option value='ger'>German</option>
+              <option value='fr'>French</option>
+            </select>
+            <select>
+              <option hidden selected>Theme</option>
+              <option value='light'>Light</option>
+              <option value='dark'>Dark</option>
+            </select>
+            <button>Help</button>
+            <button onClick={handleSignOut}>Logout</button>
+            <Avatar src={avatar} alt={username} />
+          </div>
+        </div>
+        <div className={styles.navbar_botnav}>
+          <button>Overview</button>
+          <button>Templates</button>
+          <button>Changes</button>
+          <button>Planning</button>
+          <button>Settings</button>
+        </div>
+      </motion.div>
+    </>
+  )
+}
+
+export default HomeNavbar
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 import Link from 'next/link'
+import Head from 'next/head'
+
+import NavbarDashboard from '../components/navbar/NavbarDashboard'
+
+import useUser from '../../hooks/user/useUser'
 
 function index() {
+  const user = useUser()
+  
   return (
-    <div>Index</div>
+    <>
+      <Head>
+          <title>Dashboard - PadelHost</title>
+          <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className=''>
+          {
+              user && 
+              <NavbarDashboard username={user.username} avatar={user.avatar} />
+          }
+      </div>
+    </>
   )
 }
 
